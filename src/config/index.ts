25b4486c3b05491c6d998b6da71ce733bc684529
[Francisco Omedes] <fomedes.dev@gmail.com>
2026-03-27 01:48:49 +0000

    Added CanonicalURL

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,7 +20,7 @@ export const SITE_CONFIG: SiteConfig = {
     { text: "Github", href: "https://github.com/fomedes" },
   ],
   socialImage: "/zen-og.png",
-  canonicalURL: "https://astro-zen.vercel.app", /// Update
+  canonicalURL: "https://fomedes.dev/",
 };
 
 export const SITE_CONTENT: SiteContent = {
